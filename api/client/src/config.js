945add39bba44e81cf170aacaaa50f766b968f51
Alexander Lee Smith <63920477+lapsangsouchy@users.noreply.github.com>
2022-02-04 15:31:37 -0500

    Fixes Axios Instance Variable

--- api/client/src/config.js
+++ api/client/src/config.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: 'https://asidesblogbeta.herokuapp.com/',
+  baseURL: 'https://asidesblogbeta.herokuapp.com',
 });
